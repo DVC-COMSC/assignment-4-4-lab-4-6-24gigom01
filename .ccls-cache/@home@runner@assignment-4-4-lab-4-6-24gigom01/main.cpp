26d9fb57bf084e94052dc1e125246248b4583f4f
Giovanni Gomez <112590690+24gigom01@users.noreply.github.com>
2022-10-05 23:17:22 +0000

    The problems

--- .ccls-cache/@home@runner@assignment-4-4-lab-4-6-24gigom01/main.cpp
+++ .ccls-cache/@home@runner@assignment-4-4-lab-4-6-24gigom01/main.cpp
@@ -9,15 +9,21 @@ int main()
   cout << "3. Area of a cylinder\n";
   cout << "4. None of them!\n";
   cin >> selection;
-  if (selection == 1)
+ switch (selection) {
+  case 1:
     cout << "Pi times radius squared\n";
-  else if (selection == 2)
+    break;
+  case 2:
     cout << "Length times width\n";
-  else if (selection == 3)
+    break;
+  case 3:
     cout << "Pi times radius squared times height\n";
-  else if (selection == 4)
+    break;
+  case 4:
     cout << "Well okay then, good bye!\n";
-  else
-    cout << "Not good with numbers, eh?\n";
+    break;
+  default:
+    cout << "Not good with numbers\n";
+  }
   return 0;
 }
